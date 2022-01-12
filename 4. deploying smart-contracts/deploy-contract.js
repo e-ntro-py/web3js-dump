f9ff33e51cfd014203a761c97824678a65e3be4a
Priyansh Vyas <f20201388@goa.bits-pilani.ac.in>
2022-01-13 03:40:20 +0530

    deployed, interacted with nordcoin smart-contract

---
+++ 4. deploying smart-contracts/deploy-contract.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const bytecode = require('./bytecode.json')
+const Tx = require('ethereumjs-tx').Transaction;
+const Web3 = require('web3');
+const url = `https://ropsten.infura.io/v3/${process.env.API_KEY}`;
+const web3 = new Web3(url);
+
+const address1 = '0x669b140D0ca95F45f4Cf87255bE02E4583FF04CE';
+const privateKey1Buffer = Buffer.from(process.env.PRIVATE_KEY_1, 'hex');
+
+web3.eth.getTransactionCount(address1, (err, txCount) => {
+    
+    // Build the transaction with the smart contract data
+    const txObject = {
+        nonce: web3.utils.toHex(txCount) ,
+        gasLimit : web3.utils.toHex(1000000) ,
+        gasPrice : web3.utils.toHex(web3.utils.toWei('10','gwei')),
+        data : bytecode.code
+    }
+
+    // Signing the transaction with EthereumTX
+    const tx = new Tx(txObject,  {
+         chain: 'ropsten' 
+    });
+    tx.sign(privateKey1Buffer);
+
+    const serializedTx = tx.serialize();
+    const raw = '0x' + serializedTx.toString('hex');
+
+    // Broadcast the transaction using Web3
+    web3.eth.sendSignedTransaction(raw, (err, txHash) => {
+        console.log(`txHash : ${txHash}`);
+    })
+
+})
---
+++ 4. deploying smart-contracts/interaction.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const Web3 = require('web3');
+const url = `https://ropsten.infura.io/v3/${process.env.API_KEY}`;
+const web3 = new Web3(url);
+const abi = require('./abi.json');
+
+const contractAddress = '0xb815a5f0dd05Ad0D89304D51cb03aff772134432';
+const nordcoinContract = new web3.eth.Contract(abi, contractAddress);
+
+nordcoinContract.methods.name().call((err, name) => {
+    console.log(`Coin name : ${name}`);
+});
+
+nordcoinContract.methods.symbol().call((err, symbol) => {
+    console.log(`Symbol : ${symbol}`);
+});
+
+nordcoinContract.methods.max_nordcoins().call((err, amount) => {
+    console.log(`Total nordcoins : ${amount}`);
+});
